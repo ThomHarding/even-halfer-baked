99b7dbfea7f4d06892f6a2367259e97d6bd22f7d
THOMAS HARDING <hardingt604@gmail.com>
2022-04-25 19:57:52 -0600

    added required elements to main, zodiaccard, app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+{/* import and use the Header. Main, and Footer components here */}
+import Header from './Header/Header.js';
+import Footer from './Footer/Footer.js';
+import Main from './Main/Main.js';
+import { zodiac } from './data.js';
 {/* import the zodiac array */}
 
 function App() {
   return (
     <div className="App">
-      {/* import and use the Header. Main, and Footer components here */}
+      <Header name='Zodiac Time'/>
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Footer year='2004'/>
       {/* the Footer component needs a year passed in as the `year` prop */}
+      <Main zodiacs={zodiac}/>
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
     </div>
   );
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import backgroundImg from '../background.png';
+{/* import the ZodiacCard component */}
+import ZodiacCard from '../ZodiacCard/ZodiacCard';
+{/* get the zodiac array from props */}
+import { zodiac } from '../data';
 import './Main.css';
 
 export default function Main() {
   return (
-    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      {/* import the ZodiacCard component */}
-      {/* get the zodiac array from props */}
-      {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
-      {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}> {
+      zodiac.map((zodiac) => 
+        <ZodiacCard
+          key={zodiac.name}
+          zodiac={zodiac} />
+      )
+    }
+    {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
+    {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
     </main>
   );
 }
